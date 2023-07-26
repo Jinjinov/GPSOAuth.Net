15fd02f63a1fb92d0fc1b2f5477f758f18eeaf70
Urban <dzindzinovic@hotmail.com>
2023-07-26 22:46:47 +0200

    Initial commit

--- GPSOAuth.Net/GPSOAuth.cs
+++ GPSOAuth.Net/GPSOAuth.cs
@@ -1,9 +1,23 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace GPSOAuth.Net
 {
-    public class GPSOAuth
+    public class OAuth
     {
+        public Dictionary<string, string> PerformMasterLogin(string email, string password, string deviceId)
+        {
+            return new Dictionary<string, string>();
+        }
 
+        public Dictionary<string, string> PerformOAuth(
+            string email,
+            string masterToken,
+            string deviceId,
+            string[] service,
+            string app,
+            string client_sig)
+        {
+            return new Dictionary<string, string>();
+        }
     }
 }
